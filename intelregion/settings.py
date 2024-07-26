4e10eb88a0ad3554b8c0e284faa8eda640830eeb
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2024-07-26 09:47:26 +0100

    added whitenoise

--- intelregion/settings.py
+++ intelregion/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "intelregion.urls"
@@ -125,6 +126,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Internationalization
